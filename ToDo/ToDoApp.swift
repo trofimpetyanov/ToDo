c6bc995bdcd0d6c20060a413a18bde84738f1155
Trofim Petyanov <trofimpetyanov@icloud.com>
2024-06-19 23:17:28 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Trofim Petyanov on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
