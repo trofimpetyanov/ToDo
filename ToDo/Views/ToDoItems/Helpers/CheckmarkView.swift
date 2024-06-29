05b820e2d40209f9a2c253831c2c0d7e98021bb8
Trofim Petyanov <trofimpetyanov@icloud.com>
2024-06-29 07:41:09 +0300

    Feature: Implement Primary Screens
    – Implement ToDoItems List, ToDoItemDetail.
    – Implement helper views ListHeader, ListRow, CheckmarkView.

---
+++ ToDo/Views/ToDoItems/Helpers/CheckmarkView.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+struct CheckmarkView: View {
+    @Binding var toDoItem: ToDoItem
+    
+    let size: CGFloat = 24
+    let onCompleted: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation(.interactiveSpring) {
+                updateToDoItem()
+            }
+        } label: {
+            if toDoItem.isCompleted {
+                Circle()
+                    .fill(.green)
+                    .frame(width: size, height: size)
+                    .overlay {
+                        Image(systemName: "checkmark")
+                            .resizable()
+                            .fontWeight(.black)
+                            .foregroundStyle(.white)
+                            .frame(width: size * 0.5, height: size * 0.5)
+                    }
+            } else {
+                Circle()
+                    .fill(toDoItem.importance == .important ? .red.opacity(0.1) : .clear)
+                    .strokeBorder(lineWidth: 1.5)
+                    .foregroundStyle(toDoItem.importance == .important ? .red : .primary.opacity(0.2))
+                    .frame(width: size, height: size)
+            }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+    
+    private func updateToDoItem() {
+        toDoItem = ToDoItem(
+            id: toDoItem.id,
+            text: toDoItem.text,
+            importance: toDoItem.importance,
+            dueDate: toDoItem.dueDate,
+            color: toDoItem.color,
+            isCompleted: !toDoItem.isCompleted,
+            dateCreated: toDoItem.dateCreated,
+            dateEdited: toDoItem.dateEdited
+        )
+        
+        onCompleted()
+    }
+}
+
+#Preview {
+    CheckmarkView(toDoItem: .constant(FileCache.mock[1]), onCompleted: {})
+}
