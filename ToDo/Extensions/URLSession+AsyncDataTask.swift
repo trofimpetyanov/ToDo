bd857796ff19318382590c89f573d71070ad2cd7
Trofim Petyanov <trofimpetyanov@icloud.com>
2024-07-13 16:22:52 +0400

    Feature: Add Logging to dataTask.

--- ToDo/Extensions/URLSession+AsyncDataTask.swift
+++ ToDo/Extensions/URLSession+AsyncDataTask.swift
@@ -1,10 +1,12 @@
 import Foundation
+import LoggerPackage
 
 extension URLSession {
     func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
         return try await withCheckedThrowingContinuation { continuation in
             guard !Task.isCancelled else {
                 continuation.resume(throwing: CancellationError())
+                Logger.logInfo("Data Task was cancelled.")
                 
                 return
             }
@@ -12,10 +14,16 @@ extension URLSession {
             let task = self.dataTask(with: urlRequest) { data, response, error in
                 if let error = error {
                     continuation.resume(throwing: error)
+                    
+                    Logger.logError("Data Task error: \(error.localizedDescription)")
                 } else if let data = data, let response = response {
                     continuation.resume(returning: (data, response))
+                    
+                    Logger.logInfo("Data Task completed successfully.")
                 } else {
                     continuation.resume(throwing: URLError(.badServerResponse))
+                    
+                    Logger.logError("Data Task error: Bad server response.")
                 }
             }
             
